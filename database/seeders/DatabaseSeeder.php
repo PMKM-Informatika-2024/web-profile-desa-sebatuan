e845bfde5564c4c3bb2d8d994cbe43884a8be143
shidiqfzr <shidiqfazar28@gmail.com>
2024-12-10 08:38:53 +0700

    change username and pw

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::create([
-            'username' => 'admin_pemangkat-kota',
-            'password' =>bcrypt('PemangkatKota2024!')
+            'username' => 'admin_sebatuan',
+            'password' =>bcrypt('Sebatuan2024!')
         ]);
         Profildesa::create([
             'sejarah_desa' => 'Sejarah singkat desa ini bermula dari...',
